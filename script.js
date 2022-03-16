9a69440165d91ae6e5dd16bb96ade4e1bdd92e86
Joseph Valverde <jvalverdec@pucp.edu.pe>
2022-03-16 13:34:45 +0000

    from theia environment

--- script.js
+++ script.js
@@ -10,12 +10,20 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     
-    // Calculating simple interest
+    // Calculating simple interest and total amount
     var interest = principal * years * rate / 100;
+    // To decimals round
+    interest = +(interest.toFixed(2));
+
+    // Calculating actual year in the future
     var year = new Date().getFullYear()+parseInt(years);
     
     // Showing output
-    document.getElementById("result").innerHTML="If you deposit "+principal+", \<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML=
+    "If you deposit <mark>"+principal+"</mark>,\<br\>"+
+    "at an interest rate of <mark>"+rate+"%</mark>\<br\>"+
+    "You will receive an amount of <mark>"+interest+"</mark>,\<br\>"+
+    "in the year <mark>"+year+"</mark>\<br\>";
 }
 
 function updateRate()
